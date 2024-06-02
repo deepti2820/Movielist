f814e811f20b78b3ca5b4169d492de0361885e92
deepti2820 <165156846+deepti2820@users.noreply.github.com>
2024-06-02 13:17:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ const User=require("./models/user")
 const mongoSanitize = require('express-mongo-sanitize');
 const MongoStore=require('connect-mongo')
 const helmet=require("helmet");
-const PlayList=require("./models/playlist")
+const PlayList=require("./models/playlist");
+const {isLoggedIn}=require("./middleware.js")
 //routes
 
 const authRouter=require("./routes/auth");
+const movieRouter=require("./routes/movies.js")
 
 
-
-const dbUrl=process.env.dbUrl;
+const dbUrl=process.env.dbUrl || "mongodb+srv://deeptisinghal2003:HwKBB5JLzzEzo18c@movielist.i7guy2f.mongodb.net/?retryWrites=true&w=majority&appName=Movielist";
 mongoose.connect(dbUrl)
 .then(()=>{
     console.log("DB Connected");
@@ -38,18 +39,11 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"public")));
 app.use(methodOverride('_method'));
 
-app.use(
-    mongoSanitize({
-      replaceWith: '_',
-    }),
-);
-app.use(
-    helmet({
-      contentSecurityPolicy: false,
-      xDownloadOptions: false,
-    }),
-);  
-const secret=process.env.SECRET
+mongoose.set('strictQuery', true);
+
+
+
+const secret=process.env.SECRET || "weneedsomebettersecret"
 
 const store =MongoStore.create({
     secret:secret,
@@ -92,87 +86,10 @@ app.get("/",(req,res)=>{
 });
 
 
-app.get("/homePage",async (req,res)=>{
-    const user=await User.findById(req.user._id);
-    const action=await user.populate('action')
-    const horror=await user.populate('horror')
-    const fantasy=await user.populate('fantasy')
-    const use=await user.populate('drama')
-    const comedy=await user.populate('comedy')
-    res.render("homePage",{use})
-})
 
-app.get('/search', async (req, res) => {
-    const query = req.query.movie;
-    const movies=await  fetch(`http://www.omdbapi.com/?s=${query}&page=1&apikey=869b5be3`)
-    .then((res)=>res.json())
-    .then((data)=>{
-      return data
-    })
-    const moviesSearch=movies.Search
-    res.render("movies",{moviesSearch});
-});
-
-app.post("/movie/:id",async(req,res)=>{
-    const {id}=req.params;
-    const movie=await fetch(`http://www.omdbapi.com/?i=${id}&page=2&apikey=869b5be3`)
-    .then((res)=>res.json())
-    .then((data)=>{
-      return data
-    })
-    const playList=new PlayList({ name: movie.Title,image:movie.Poster,year:movie.Year });
-    const genre =movie.Genre;
-    const user=await User.findById(req.user._id)
-    if(genre.indexOf('Action')>=0){
-        user.action.push(playList);
-    }else if(genre.indexOf('Drama')>=0){
-        user.drama.push(playList);
-    }else if(genre.indexOf('Horror')>=0){
-        user.horror.push(playList);
-    }else if(genre.indexOf('Comedy')>=0){
-        user.comedy.push(playList);
-    }else{
-        user.fantasy.push(playList);
-    }
-    await playList.save();
-    await user.save();
-    
-    res.redirect("/homePage");
-})
-
-app.get("/:movie/playlist",async (req,res)=>{
-    const {movie}=req.params;
-    const user=await User.findById(req.user._id);
-    const movies=await user.populate(movie)
-    console.log(movies)
-    res.render("playlist.ejs",{movies,movie});
-})
-
-app.delete("/:uid/:type/:id",async(req,res)=>{
-    const {uid,type,id}=req.params;    
-    async function removePlaylistFromUser(userId, playlistId, genre) {
-        try {
-          let update = {};
-          update[genre] = playlistId;
-          await User.findByIdAndUpdate(userId, {
-            $pull: update
-          });
-      
-          console.log("Playlist removed successfully");
-          await PlayList.findByIdAndDelete(playlistId);
-
-        } catch (err) {
-          console.error("Error removing playlist:", err);
-        }
-      }
-      
-      removePlaylistFromUser(uid, id, type);
-      res.redirect("/homePage")
-
-})
 app.use(authRouter);
 
-
+app.use(movieRouter)
 
 
 
